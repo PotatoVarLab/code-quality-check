242bc81e22e39d5317220a2e832c7aa1a751989e
PotatoVarLab <andrew_winston@126.com>
2021-02-26 18:50:06 +0800

    Update start.py

--- start.py
+++ start.py
@@ -5,7 +5,7 @@ from __future__ import division
 from __future__ import print_function
 
 
-vocab = {
+VOCAB = {
   "q": 1,
   "w": 2,
   "e": 3,
@@ -17,6 +17,5 @@ def convert_string_to_id(text=''):
     ids = []
     for i in text:
         if i in ['q','w','e','r']:
-            id = vocab.get(i, 0)
-            ids.append(id)
+            ids.append(VOCAB.get(i, 0))
     return ids
